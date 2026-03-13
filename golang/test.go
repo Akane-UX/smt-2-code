014287d7252c922ad91e6c79a8b97a0d1a4b4c34
Akane-UX <rtempest922@gmail.com>
2026-03-13 10:01:15 +0700

    Refactor test.go to initialize names array and improve output formatting

--- golang/test.go
+++ golang/test.go
@@ -5,10 +5,20 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, Go!")
 	var namadepan, namaakhir, name string
+	var names [4]string
+	names[0] = "trafalgar"
+	names[1] = "d"
+	names[2] = "water"
+	names[3] = "law"
 	namadepan = "avril"
 	namaakhir = "cihuy"
 	fmt.Scanln(&name)
 
 	fmt.Println("namaku", namadepan, namaakhir, name)
+
+	
+
+
+fmt.Println(names[0], names[1], names[2], names[3])
 
 }
