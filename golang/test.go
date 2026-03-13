40b638f6b658c87b1d723e139b5756e5b75e5a2d
Akane-UX <rtempest922@gmail.com>
2026-03-13 11:03:58 +0700

    Update test.go to change names array initialization

--- golang/test.go
+++ golang/test.go
@@ -6,7 +6,7 @@ func main() {
 	fmt.Println("Hello, Go!")
 	var namadepan, namaakhir, name string
 	var names [4]string
-	names[0] = "trafalgar"
+	names[0] = "tr"
 	names[1] = "d"
 	names[2] = "water"
 	names[3] = "law"
